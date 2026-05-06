9c1c2d40ebc57e1639850551a8de281a23c83ea2
Gabriel Balbinot <gabrielfdr.balbinot@gmail.com>
2026-05-06 08:37:26 -0300

    Menu em progesso

--- menu.asm
+++ menu.asm
@@ -5,6 +5,10 @@
 	_menu.2: .asciz "8- Máximo\n9- Minímo\n0- Sair\n"
 .text
 	
+	addi sp sp -4
+	sw s0 0(sp) # armazenando s0 
+	
+	
 	menu:
 		j printMenu
 	
@@ -28,4 +32,34 @@ inputMenu:
 	ecall
 	
 	beqz a0 end_menu
+	
+	li t6 1
+	beq a0 t6 insert
+	
+	li t6 2
+	beq a0 t6 delete
+	
+	li t6 3
+	beq a0 t6 search
+	
+	li t6 4
+	beq a0 t6 preorder
+	
+	li t6 5
+	beq a0 t6 inorder
+	
+	li t6 6
+	beq a0 t6 postorder
+	
+	li t6 7
+	beq a0 t6 arvoreToda
+	
+	li t6 7
+	beq a0 t6 max
+	
+	li t6 7
+	beq a0 t6 min
+	
+	
 	j printMenu
+
